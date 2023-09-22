66312545b20700b652bef25216c1f540f3b0cb70
haechi <epiclog@naver.com>
2023-09-22 14:14:36 +0900

    Update stockApis.js

--- src/apis/stockApis.js
+++ src/apis/stockApis.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
-const SERVER_URL = "http://localhost:3001";
+// const SERVER_URL = "http://localhost:3001";
+const SERVER_URL = "https://raw.githubusercontent.com/kimhaech/shinhan-front/main/src/data/db.json";
 
 // 해외주식 이벤트 리스트 GET API 호출
 export const getStockGlobalEvents = async () => {
-    const response = await axios.get(`${SERVER_URL}/globalStockEvents`);
-    // return axios
-    //     .get(`${SERVER_URL}/globalStockEvents`)
-    //     .then((response) => response.data);
-    return response.data;
+    // before
+    // const response = await axios.get(`${SERVER_URL}/globalStockEvents`);
+    // return response.data;
+
+    // after
+    return await axios
+        .get(`${SERVER_URL}`)
+        .then((response) => response.data.globalStockEvents);
 };
